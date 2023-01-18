0c3f3c885c6bfe16e7debc2b59145f7b6710df9c
Alex Krysov <krysov-95@mail.ru>
2023-01-19 04:10:14 +0600

    added hw18

---
+++ src/test/java/com/krysov/tests/ReqresInTests.java
@@ -0,0 +1,146 @@
+package com.krysov.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTests {
+    @Test
+    void loginTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unSupportedMediaTypeTest() {
+        given()
+                .log().uri()
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(415);
+    }
+
+    @Test
+    void missingEmailOrUsernameTest() {
+        given()
+                .log().uri()
+                .body("123")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void missingPasswordTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void createTest() {
+        String data = "{ \"name\": \"Alex\", \"job\": \"QA\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("Alex"));
+    }
+
+    @Test
+    void patchTest() {
+        String data = "{ \"name\": \"Alex\", \"job\": \"QA\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("Alex"));
+    }
+
+    @Test
+    void singleUserTest() {
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.email", is("janet.weaver@reqres.in"));
+    }
+
+    @Test
+    void singleResourceTest() {
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/unknown/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.color", is("#C74375"));
+    }
+
+    @Test
+    void deleteTest() {
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+}
